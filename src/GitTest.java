3e82a6a6339026b2812cc75b4f16a1ddb90bdbd9
Anders <andersravn94@gmail.com>
2023-09-22 09:26:28 +0200

    More text buddy

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,8 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("Sgt Asswipe xD");
+        System.out.println("Jeg tilføjer mere tekst, omegalul");
+        System.out.println("Sut den");
     }
 
 }
